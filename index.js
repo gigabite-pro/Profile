9ff5cae7c5c5e15d6ffd017b145e4b4523e6eee1
Vaibhav <59077631+gigabite-pro@users.noreply.github.com>
2020-05-29 14:50:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-let like = document.getElementsByClassName("on_click");
-let para = document.getElementById("show_me");
+let pic = document.getElementById("photo");
+let like = document.getElementById("on_click");
+let para = document.getElementById("hidden");
+let contain = document.getElementById("container");
 
-like.addEventListener("click",show);
-
+like.addEventListener("click", show);
 function show() {
-  para.style.display = "inline-block";
+  para.style.visibility = "visible";
 }
+
+contain.addEventListener("mouseover", opacity)
+function opacity() {
+  pic.style.opacity = "0.7";
+  like.style.visibility = "visible";
+}
+
+contain.addEventListener("mouseout", reopacity)
+function reopacity(){
+  pic.style.opacity = "1";
+  like.style.visibility = "hidden";
+}
+
+like.addEventListener("mouseover", opacity);
+
+
+
